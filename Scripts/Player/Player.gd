e977c9233d85e4a12306f377e85abe2da0266b52
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-11-05 02:08:45 -0300

    Animação de 'run' implementada no player

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -9,3 +9,15 @@ export var gravity = 300
 
 func _ready():
 	pass
+
+
+func _process(delta):
+	if Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.animation = "run"
+		$AnimatedSprite.flip_h = true
+	elif Input.is_action_pressed("ui_right"):
+		$AnimatedSprite.animation = "run"
+		$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.animation = "idle"
+		
