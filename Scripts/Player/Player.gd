12bf9b3bb1df3de96b88db4bb6b3e1d5103614d8
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-11-05 01:50:44 -0300

    Animação 'idle' do player adicionada ao seu objeto

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -9,24 +9,3 @@ export var gravity = 300
 
 func _ready():
 	pass
-
-func _process(delta):
-	if Input.is_action_pressed("ui_left"):
-		move.x = -speedX
-		$AnimatedSprite.animation = "walking"
-		$AnimatedSprite.flip_h = true
-	elif Input.is_action_pressed("ui_right"):
-		move.x = speedX
-		$AnimatedSprite.animation = "walking"
-		$AnimatedSprite.flip_h = false
-	else:
-		move.x = 0
-		$AnimatedSprite.animation = "idle"
-		
-	if is_on_floor():
-		move.y = 0
-		if Input.is_action_pressed("ui_accept"):
-			move.y = -jumpForce
-			
-	move.y += gravity * delta
-	move_and_slide(move, UP)
