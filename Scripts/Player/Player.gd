b94348e6397d0fbd5ba4b4925e7ed99e793a20d0
JP <cookiebaker145@gmail.com>
2019-11-06 16:24:04 -0300

    Script de movimento criado, colisores dos personagens alterados
    Consertar valores de pulo e gravidade

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,23 +1,26 @@
 extends KinematicBody2D
 
-const UP = Vector2(0,-1)
-var move = Vector2()
-
-export var speedX = 100
-export var jumpForce = 200
-export var gravity = 300
+const general_movement=preload("../Movement.gd")
+onready var player_movement = general_movement.new()
 
 func _ready():
 	pass
 
-
 func _process(delta):
+	player_movement.move.y=player_movement.gravity
+	if is_on_floor():
+		player_movement.move.y=0;
+	if Input.is_action_pressed("ui_up"):
+		player_movement.jump()
 	if Input.is_action_pressed("ui_left"):
+		player_movement.move("right")
 		$AnimatedSprite.animation = "run"
 		$AnimatedSprite.flip_h = true
 	elif Input.is_action_pressed("ui_right"):
+		player_movement.move("left")
 		$AnimatedSprite.animation = "run"
 		$AnimatedSprite.flip_h = false
 	else:
 		$AnimatedSprite.animation = "idle"
-		
+		player_movement.move.x=0
+	move_and_slide(player_movement.move, player_movement.UP)
