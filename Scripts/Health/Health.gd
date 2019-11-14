c70c088c73c791e98269f248028266eaad4b9047
JP <cookiebaker145@gmail.com>
2019-11-14 00:06:09 -0300

    Cena criada, contendo os nodes e scripts "Health" e "HealthController"
    HealthController ainda vazio. Talvez esse script deva ser desenvolvido em conjunto com a mecânica de ataque.

---
+++ Scripts/Health/Health.gd
@@ -0,0 +1,42 @@
+extends Node
+
+export (int) var current_hp = 1
+export (int) var max_hp = 1
+
+func set_max_hp(value:int):
+	max_hp=value
+	if current_hp>max_hp:
+		current_hp=max_hp
+
+func set_current_hp(value:int):
+	if current_hp+value>max_hp:
+		return false
+	else:
+		current_hp=value
+
+func reduce_current_hp(value:int):
+	if value>current_hp:
+		current_hp=0
+	else:
+		current_hp-=value
+
+func reduce_max_hp(value:int):
+	if value>=max_hp:
+		return false
+	else:
+		max_hp-=value
+		if current_hp>max_hp:
+			current_hp=max_hp
+
+
+func increase_current_hp(value:int):
+	if value+current_hp>max_hp:
+		current_hp=max_hp
+	else:
+		current_hp+=value
+
+func increase_max_hp(value:int):
+	max_hp+=value
+
+func reset_hp():
+	current_hp=0
---
+++ Scripts/Health/HealthController.gd
@@ -0,0 +1,6 @@
+extends Node
+
+#func _ready():
+#	pass
+#func _process(delta):
+#	pass
