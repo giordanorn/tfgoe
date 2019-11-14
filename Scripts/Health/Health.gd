00058a8b2bcc999fac0b8d20079e96b32585d6fc
JP <cookiebaker145@gmail.com>
2019-11-14 12:05:44 -0300

    Modificações no script de vida e exclusão do HealthController

--- Scripts/Health/Health.gd
+++ Scripts/Health/Health.gd
@@ -4,40 +4,57 @@ export (int) var current_hp = 1
 export (int) var max_hp = 1
 
 func set_max_hp(value:int):
-	max_hp=value
-	if current_hp>max_hp:
-		current_hp=max_hp
-
-func set_current_hp(value:int):
-	if current_hp+value>max_hp:
-		current_hp=max_hp
+	if value<0:
+		print_debug("tentativa de setar o hp maximo para um valor negativo")
 	else:
-		current_hp=value
-
-func reduce_current_hp(value:int):
-	if value>current_hp:
-		current_hp=0
-	else:
-		current_hp-=value
-
-func reduce_max_hp(value:int):
-	if value>=max_hp:
-		return false
-	else:
-		max_hp-=value
+		max_hp=value
 		if current_hp>max_hp:
 			current_hp=max_hp
 
+func set_current_hp(value:int):
+	if value<0:
+		print_debug("tentativa de setar o hp para um valor negativo")
+	else:
+		if current_hp+value>max_hp:
+			current_hp=max_hp
+		else:
+			current_hp=value
+
+func reduce_current_hp(value:int):
+	if value<0:
+		print_debug("tentativa de reduzir o hp com um valor negativo")
+	else:
+		if value>current_hp:
+			current_hp=0
+		else:
+			current_hp-=value
+
+func reduce_max_hp(value:int):
+	if value<0:
+		print_debug("tentativa de reduzir o hp maximo com um valor negativo")
+	else:
+		if value>=max_hp:
+			print_debug("tentativa de reduzir o hp para um valor <0")
+		else:
+			max_hp-=value
+			if current_hp>max_hp:
+				current_hp=max_hp
 
 func increase_current_hp(value:int):
-	if value+current_hp>max_hp:
-		current_hp=max_hp
+	if value<0:
+		print_debug("tentativa de aumentar o hp com um valor negativo")
 	else:
-		current_hp+=value
+		if value+current_hp>max_hp:
+			current_hp=max_hp
+		else:
+			current_hp+=value
 
 func increase_max_hp(value:int):
-	max_hp+=value
-	current_hp+=value
+	if value<0:
+		print_debug("tentativa de aumentar o hp maximo por um valor negativo")
+	else:
+		max_hp+=value
+		current_hp+=value
 
 func reset_hp():
 	current_hp=0
