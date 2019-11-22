bdba754c72c1f5b2722d481ff2a44d50b2ddfa78
JP <cookiebaker145@gmail.com>
2019-11-22 14:22:58 -0300

    ContactAttack implementado no orcPursuer

--- Scripts/Attack/ContactAttack.gd
+++ Scripts/Attack/ContactAttack.gd
@@ -1,7 +1,17 @@
-extends Area2D
+extends Node2D
+
+export var strength = 0
+onready var character = get_parent()
 
 func _ready():
-	pass # Replace with function body.
+	$HitArea/Shape.set_shape(character.get_node("CollisionShape2D").get_shape())
+	$HitArea/Shape.position = character.get_node("CollisionShape2D").position
+
+func _on_HitArea_body_entered(target):
+	if target.is_in_group("player"):
+		print (character.nickname, ": hit ", target.nickname)
+		target.take_damage(strength)
+		pass
 
 func _process(delta):
 	pass
