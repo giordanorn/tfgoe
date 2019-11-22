5677c80cd1a7b6c788e15c2722fe2aa08615c16c
JP <cookiebaker145@gmail.com>
2019-11-22 14:47:45 -0300

    ContactAttack corrigido e implementado em todos os monstros

--- Scripts/Attack/ContactAttack.gd
+++ Scripts/Attack/ContactAttack.gd
@@ -6,6 +6,10 @@ onready var character = get_parent()
 func _ready():
 	$HitArea/Shape.set_shape(character.get_node("CollisionShape2D").get_shape())
 	$HitArea/Shape.position = character.get_node("CollisionShape2D").position
+	character.set_collision_layer(2)
+	character.set_collision_mask(2)
+	print(character.get_collision_layer())
+	print(character.get_collision_mask())
 
 func _on_HitArea_body_entered(target):
 	if target.is_in_group("player"):
