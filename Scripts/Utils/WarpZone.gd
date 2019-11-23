140fe85180aff9615a43fbc202e23ffc5dfca1f8
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-11-23 17:30:08 -0300

    Pequenos ajustes

--- Scripts/Utils/WarpZone.gd
+++ Scripts/Utils/WarpZone.gd
@@ -2,16 +2,9 @@ extends Node2D
 
 export(String, FILE) var scene_path
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func _on_EffectArea_body_entered(body):
+func _on_Area2D_body_entered(body):
 	"""
 	Muda a cena para `scene_path`
 	"""
-	get_tree().change_scene(scene_path)
+	if body.is_in_group("player"):
+		get_tree().change_scene(scene_path)
