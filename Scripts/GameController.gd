d0b7dafb760a46cc7d88e4436a66b772beb32ee8
JP <cookiebaker145@gmail.com>
2019-11-25 06:31:30 -0300

    Seleção de personagem implementada

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+enum PLAYER_CHARACTER{
+	WIZARD=0,
+	KNIGHT=1,
+	ELF=2
+}
+
+export (int) var score = 0
+export var selected_character = PLAYER_CHARACTER.ELF
+
 """
 Aqui devem ser guardadas as variáveis globais jogo.
 """
---
+++ Scripts/Stage.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+onready var game_info = get_node("/root/GameController")
+var elf_info = "res://Characters/Players/Elf.tscn"
+var wiz_info = "res://Characters/Players/Wizard.tscn"
+var knt_info = "res://Characters/Players/Knight.tscn"
+var character
+
+func _ready():
+	if game_info.selected_character == 2:
+		character = load("res://Characters/Players/Elf.tscn") as PackedScene
+	elif game_info.selected_character == 1:
+		character = load("res://Characters/Players/Knight.tscn") as PackedScene
+	elif game_info.selected_character == 0:
+		character = load("res://Characters/Players/Wizard.tscn") as PackedScene
+	else:
+		print("invalid character choice")
+	character=character.instance()
+	self.add_child(character)
+	character.position = Vector2(16,70)
